ef383a356b684f4a4bd714f25e51b3ddc780c9f9
Josue Sebastiao <josuesalazaku@gmail.com>
2023-09-17 14:06:32 +0200

    Random Quote Generator

--- src/Components/RandomQuote/RandomQuotes.jsx
+++ src/Components/RandomQuote/RandomQuotes.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import './RandomQuotes.css'
 const RandomQuote = () => {
   return (
-    <div>
-
+   
+    const [quote, setQuote] = useState({
+        text: "Throughout Heaven and Earth, I alone am honored",
+        author: "Gojo Satoru",
+    });
+   
+   <div className='container'>
+     <div className="quote">{quote.text}</div>
+     <div>
+        <div className="line"></div>
+        <div className="bottom">
+            <div className="author">{quote.author}</div>
+        </div>
+     </div>
     </div>
   ) 
      
